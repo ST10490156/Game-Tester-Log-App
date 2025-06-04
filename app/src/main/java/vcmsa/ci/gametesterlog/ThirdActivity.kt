390f313ed357a63db86e0a7e3e5a8df17ada0c8b
Tshinguli Kapenda <ST10490156@vcconnect.edu.za>
2025-06-04 11:38:53 +0200

    update: initial code

--- app/src/main/java/vcmsa/ci/gametesterlog/ThirdActivity.kt
+++ app/src/main/java/vcmsa/ci/gametesterlog/ThirdActivity.kt
@@ -5,24 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.EditText
-import android.widget.Button
-import 
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var btnStart: Button
-    private lateinit var btnExit: Button
-    private lateinit var etStudentName: EditText
-    private lateinit var etStudentNumber: EditText
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_third)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
